6c2583ae7a5ca33f60391da925a6042b7600810a
Robert L Kelly <robert.l.kelly3@gmail.com>
2019-02-03 22:30:45 -0500

    testing server performance

--- src/server.rs
+++ src/server.rs
@@ -4,7 +4,7 @@ use std::{io, thread};
 
 use futures::sync::oneshot;
 use futures::Future;
-use grpcio::{Environment, ServerBuilder};
+use grpcio::{Environment, ServerBuilder, ChannelBuilder};
 
 use accountant::proto::accounting_grpc;
 use accountant::GrpcAccountingService;
@@ -12,6 +12,14 @@ use accountant::db::PostgresDataStore;
 
 fn main() {
     let env = Arc::new(Environment::new(4));
+
+    let channel_args = ChannelBuilder::new(Arc::clone(&env))
+        .stream_initial_window_size(2 * 1024 * 1024)
+        .max_concurrent_stream(1024)
+        .max_send_message_len(32 * 1024 * 1024)
+        .max_receive_message_len(32 * 1024 * 1024)
+        .build_args();
+
     let service = accounting_grpc::create_accounting_service(
         GrpcAccountingService::new(
             PostgresDataStore::new("postgresql://accountant@localhost:26257/bank"))
@@ -20,6 +28,7 @@ fn main() {
     let mut server = ServerBuilder::new(env)
         .register_service(service)
         .bind("0.0.0.0", 3000)
+        .channel_args(channel_args)
         .build()
         .unwrap();
 
